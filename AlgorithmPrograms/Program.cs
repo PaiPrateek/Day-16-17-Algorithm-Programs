68c022f9d86f96fd32cc72564f6d6f87bb100498
Prateek Pai <paiprateek123@gmail.com>
2022-06-30 22:15:23 +0530

    Permuataion of String

--- AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/Program.cs
@@ -6,7 +6,43 @@ namespace AlgorithmPrograms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Algorithm Programs");
+            String str = "PAI";
+            int len = str.Length;
+            Console.WriteLine("All the permutations of the string are: ");
+            generatePermutation(str, 0, len);
+        }
+
+        //Function for swapping the characters 
+        public static String swapString(String a, int i, int j)
+        {
+            char[] c = a.ToCharArray();
+            char ch;
+            ch = c[i];
+            c[i] = c[j];
+            c[j] = ch;
+
+            //Converting characters from array into single string  
+            return string.Join("", c);
+        }
+
+        //Function for generating permutations
+        public static void generatePermutation(String str, int start, int end)
+        {
+            //Prints the permutations  
+            if (start == end - 1)
+                Console.WriteLine(str);
+            else
+            {
+                for (int i = start; i < end; i++)
+                {
+                    //Swapping the string 
+                    str = swapString(str, start, i);
+ 
+                    generatePermutation(str, start + 1, end);
+
+                    str = swapString(str, start, i);
+                }
+            }
         }
     }
 }
